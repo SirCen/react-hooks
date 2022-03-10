fca52f71e1f2da4d62e6f80ad412dcc57762422a
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 15:21:31 +0000

    using to add and remove event listeners

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useForm } from './useForm';
-import { Hello } from './Hello';
+//import { Hello } from './Hello';
 //Example expensive initial state
 // function expensiveInitState() {
 //   return 10;
@@ -11,25 +11,28 @@ function App() {
 
   const [values, handleChange] = useForm({email: "", password: "", firstName: ""});
   
-  const [showHello, setShowHello] = useState(true);
+  //const [showHello, setShowHello] = useState(true);
   //runs everytime the page is rendered
   //second param is dependency array of values, if they change the effect is fired again
   //if second param is empty [], then doesnt change the effect
-  // useEffect(() => {
-  //   console.log("render");
+  //can have more than one useEffect in component and fire in order
+  useEffect(() => {
+    const onMouseMove = e => {
+      console.log(e);
+    };
+    window.addEventListener('mousemove', onMouseMove);
+    //cleanup function, returned
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    }
 
-  //   //cleanup function, returned
-  //   return () => {
-  //     console.log("unmount");
-  //   }
-
-  // }, []);
+  }, []);
 
   return (
     <div>
       <>
-      <button onClick={() => setShowHello(!showHello)}>toggle</button>
-      {showHello && <Hello />}
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle</button> */}
+      {/* {showHello && <Hello />} */}
       <input name='email' value={values.email} onChange={handleChange} />
       <input placeholder="first name" name='firstName' value={values.firstName} onChange={handleChange} />
       <input type='password' name='password' value={values.password} onChange={handleChange} />
