97025808ba842c5da26206b1d2986fd8e72f5088
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 23:35:11 +0000

    basic counter useReducer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React, {useState, useMemo, useCallback} from 'react';
-import { useFetch } from './useFetch';
+import React, {useReducer} from 'react';
+
+
+// dispatch acts as a listener to the action type which is then acted on
+function reducer(state, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state+1;
+    case 'DECREMENT':
+      return state-1;
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  const {data} = useFetch("https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json");
-  
-  //this will fire this function every render
-  // with useCallback only fires once or when data is changed
-  //if no dependencies in useCallback, better to place as function outside
-  const computeLongestWord = useCallback((arr) => {
-    if (!arr) {
-      return [];
-    }
-    console.log('computing longest word');
-    let longestWord = '';
-    JSON.parse(arr).forEach(sentence => sentence.split(' ').forEach(word => {
-      if (word.length > longestWord.length) {
-        longestWord = word;
-      }
-    }) 
-    );
-    return longestWord;
-  }, []);
-
-  const longestWord = useMemo(() => computeLongestWord(data), [computeLongestWord, data]);
+  const [count, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
       <div>count: {count}</div>
-      <button onClick={() => setCount(count+1)}>increment</button>
-      <div>{longestWord}</div>
+      <button onClick={() => dispatch({type: 'INCREMENT' })}>increment</button>
+      <button onClick={() => dispatch({type: 'DECREMENT'})}>decrement</button>
     </div>
   );
 }
