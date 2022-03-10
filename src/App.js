64d0c6050581fa83375af1d4768dc8d1f215783b
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 23:19:14 +0000

    memo-ised computeLongestWord

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, {useState, useCallback} from 'react';
-import { Hello } from './Hello';
-import { Square } from './Square';
+import React, {useState, useMemo} from 'react';
+import { useFetch } from './useFetch';
+
+const computeLongestWord = (arr) => {
+  if (!arr) {
+    return [];
+  }
+  console.log('computing longest word');
+  let longestWord = '';
+  JSON.parse(arr).forEach(sentence => sentence.split(' ').forEach(word => {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }) 
+  );
+  return longestWord;
+}
 
 function App() {
 
   const [count, setCount] = useState(0);
-  const favNums = [13, 42, 69];
-  // useful when needing to prevent functions from changing value such as when using memo
-  //can pass in parameters
-  const increment = useCallback((n) => {
-    setCount(c => c+n);
-  }, [setCount]);
+  const {data} = useFetch("https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json");
+  
+  //this will fire this function every render
+  // const computeLongestWord = (arr) => {
+  //   if (!arr) {
+  //     return [];
+  //   }
+  //   console.log('computing longest word');
+  //   let longestWord = '';
+  //   JSON.parse(arr).forEach(sentence => sentence.split(' ').forEach(word => {
+  //     if (word.length > longestWord.length) {
+  //       longestWord = word;
+  //     }
+  //   }) 
+  //   );
+  //   return longestWord;
+  // }
+
+  const longestWord = useMemo(() => computeLongestWord(data), [data]);
 
   return (
     <div>
-      <Hello increment={increment}/>
       <div>count: {count}</div>
-      {favNums.map(n => {
-        return (
-          <Square increment={increment} n={n} key={n} />
-        );
-      })}
+      <button onClick={() => setCount(count+1)}>increment</button>
+      <div>{longestWord}</div>
     </div>
   );
 }
