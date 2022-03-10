1c3695286cba284681085aff2163d3f19d16a627
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 23:59:17 +0000

    todo list with useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 
 
 // dispatch acts as a listener to the action type which is then acted on
+// all logic in here
+// useState is good, but once gets more complex, 
+// useReducer is better to change multiple things in the state based on actions 
 function reducer(state, action) {
   switch(action.type) {
-    case 'INCREMENT':
-      return state+1;
-    case 'DECREMENT':
-      return state-1;
+    case 'add-todo':
+      return {
+        todos:[...state.todos, {payload: action.payload, completed: false}], 
+        todoCount: state.todoCount + 1
+      };
+    case 'toggle-todo':
+      return {
+        todos: state.todos.map((t, idx) => 
+          idx === action.idx ? {...t, completed: !t.completed} : t
+        ),
+        todoCount: state.todoCount 
+      };
     default:
       return state;
   }
@@ -15,13 +26,28 @@ function reducer(state, action) {
 
 function App() {
 
-  const [count, dispatch] = useReducer(reducer, 0);
-
+  const [{todos, todoCount}, dispatch] = useReducer(reducer, {todos: [], todoCount: 0});
+  const [text, setText] = useState();
+  
   return (
     <div>
-      <div>count: {count}</div>
-      <button onClick={() => dispatch({type: 'INCREMENT' })}>increment</button>
-      <button onClick={() => dispatch({type: 'DECREMENT'})}>decrement</button>
+      <form onSubmit={e => {
+        e.preventDefault();
+        dispatch({type: 'add-todo', payload:text});
+        setText("");
+      }}>
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+      <div>number of todos: {todoCount}</div>
+      {todos.map((t, idx) => (
+        <div 
+          key={t.payload} 
+          onClick={() => dispatch({type: 'toggle-todo', idx})} 
+          style={{textDecoration: t.completed ?  'line-through' : ""}}
+        >
+          {t.payload}
+        </div>
+      ))}
     </div>
   );
 }
