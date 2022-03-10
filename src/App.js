8473ddfd14707124d73f7fa23bedac49ebdee7d3
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 01:33:25 +0000

    created own hook using useState called useForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useForm } from './useForm';
 
 //Example expensive initial state
 // function expensiveInitState() {
@@ -8,20 +9,12 @@ import React, {useState} from 'react';
 function App() {
   // useState(() => expensiveInitState()); // will only call expensive state once, saving on computation
 
-  const [count, setCount] = useState(10);
-  const [count2, setCount2] = useState(20);
+  const [values, handleChange] = useForm({email: "", password: ""});
 
-//currently does not do merging of states so count2 will disappear
   return (
     <div>
-      <button onClick={() => {
-        setCount(c => c+1);
-        setCount2(c => c+1);
-      }}>
-        +
-      </button>
-      <div>count1: {count}</div>
-      <div>count2: {count2}</div>
+      <input name='email' value={values.email} onChange={handleChange} />
+      <input type='password' name='password' value={values.password} onChange={handleChange} />
     </div>
   );
 }
