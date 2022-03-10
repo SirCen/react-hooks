d7e841ce466685fc64a76db11c58b8fc0ac7fb91
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 01:18:05 +0000

    destructured and count incremented

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import React, {useState} from 'react';
 function App() {
   // useState(() => expensiveInitState()); // will only call expensive state once, saving on computation
 
-  const [count, setCount] = useState(10);
+  const [{count, count2}, setCount] = useState({count:10, count2: 20});
 
-
+//currently does not do merging of states so count2 will disappear
   return (
     <div>
-      <button onClick={() => setCount(currentCount => currentCount+1)}>+</button>
-      <div>{count}</div>
+      <button onClick={() => setCount(currentState => ({count: currentState.count+1}))}>
+        +
+      </button>
+      <div>count1: {count}</div>
+      <div>count2: {count2}</div>
     </div>
   );
 }
