fef55aed7a8cfe998f9003fcd3f4ab6bce08cc29
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 01:14:37 +0000

    Normal use of useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+//Example expensive initial state
+// function expensiveInitState() {
+//   return 10;
+// }
 
 function App() {
+  // useState(() => expensiveInitState()); // will only call expensive state once, saving on computation
+
+  const [count, setCount] = useState(10);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setCount(currentCount => currentCount+1)}>+</button>
+      <div>{count}</div>
     </div>
   );
 }
