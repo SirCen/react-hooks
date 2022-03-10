903039e312acc49949d1f62558f35f4e9c112e73
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2022-03-10 15:48:19 +0000

    basic useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { useFetch } from './useFetch';
 import { useForm } from './useForm';
 //import { Hello } from './Hello';
@@ -15,36 +15,14 @@ function App() {
     JSON.parse(localStorage.getItem('count'))
   );
   const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const inputRef = useRef();
   //if count changes sets count in local storage
   useEffect(() => {
     localStorage.setItem('count', JSON.stringify(count));
   }, [count]);
-  //const [showHello, setShowHello] = useState(true);
-  //runs everytime the page is rendered
-  //second param is dependency array of values, if they change the effect is fired again
-  //if second param is empty [], then doesnt change the effect
-  //can have more than one useEffect in component and fire in order
-  //http://numbersapi.com/43/trivia
-  // useEffect(() => {
-  //   const onMouseMove = e => {
-  //     console.log(e);
-  //   };
-  //   window.addEventListener('mousemove', onMouseMove);
-  //   //cleanup function, returned
-  //   return () => {
-  //     window.removeEventListener('mousemove', onMouseMove);
-  //   }
-
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log('mount1');
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log('mount2');
-  // }, []);
   
+  
+
 
   return (
     <div>
@@ -54,9 +32,12 @@ function App() {
       <>
       {/* <button onClick={() => setShowHello(!showHello)}>toggle</button> */}
       {/* {showHello && <Hello />} */}
-      <input name='email' value={values.email} onChange={handleChange} />
+      <input ref={inputRef} name='email' value={values.email} onChange={handleChange} />
       <input placeholder="first name" name='firstName' value={values.firstName} onChange={handleChange} />
       <input type='password' name='password' value={values.password} onChange={handleChange} />
+      <button onClick={() =>{
+        inputRef.current.focus();
+      }}>focus</button>
       </>
     </div>
   );
